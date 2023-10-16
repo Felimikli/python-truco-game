0fe014eda7aacbcb4538c227b852bb6cbd0d171b
Pedro Miklikowski <pedromikli@MacBook-Air-de-Pedro.local>
2023-10-16 11:14:18 -0300

    quit/play again

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import math
+import sys
 import random
 
 espada = ['e1', 'e2', 'e3', 'e4', 'e5',
@@ -28,31 +28,34 @@ def deal(numPlayer):
     return data
 
 def truco():
+    data = deal(2)
     turn = 'player'
-    hand = 0
+    playerCardsThrown = []
+    computerCardsThrown = []
+    playerCards = []
+    computerCards = []
+    playerPoints = 0
+    computerPoints = 0
 
 
-    def game(turn, hand, data):
+
+    def game(turn, handIndex, data):
+        nonlocal playerCards
+        nonlocal computerCards
         playerCards = data['player1']
         computerCards = data['player2']
-        playerCardsThrown = []
-        computerCardsThrown = []
-        playerPoints = 0
-        computerPoints = 0
-        turn = 'player'
 
-        
 
 
         def playerThrowCard(throwIndex):
+            print(f"playerThrowCard")
             nonlocal playerCards
             nonlocal computerCards
             nonlocal playerCardsThrown
             nonlocal computerCardsThrown
 
-           
-
-            playerInput = 0
+            playerInput = ''
+            msg = ''
             if throwIndex == 0:
                 msg = f"press 1 for {playerCards[0]}, 2 for {playerCards[1]}, 3 for {playerCards[2]}\n\n"
                 playerInput = input(msg)
@@ -62,6 +65,8 @@ def truco():
             elif throwIndex == 2:
                 msg = f"press 1 for {playerCards[0]}\n\n"
                 playerInput = input(msg)
+            else:
+                checkHandWinner()
 
             try:
                 player = int(playerInput)
@@ -73,6 +78,7 @@ def truco():
                     for playerCard in playerCards:
                         if player-1 == playerCards.index(playerCard):
                             playerCardsThrown.append(playerCards.pop(playerCards.index(playerCard)))
+                            computerThrowCard(computerCardsThrown)
                             checkHandWinner(throwIndex, playerCardsThrown, computerCardsThrown)
                             break
                         else:
@@ -80,48 +86,94 @@ def truco():
                 else:
                     print("Invalid input, enter 1, 2 or 3")
                     playerThrowCard(throwIndex)
+            pass
+
 
 
         def computerThrowCard(computerCardsThrown):
-            print(f"wawawawaawawa{computerCards, computerCardsThrown}")
-            computerCardsThrown.append(computerCards.pop(random.randrange(len(computerCards))))
-            print(f"wawawawaawawa{computerCards, computerCardsThrown}")
-        
+            print(f"computerThrowCard{computerCards, computerCardsThrown}")
+            if computerCards != []:
+                computerCardsThrown.append(computerCards.pop(random.randrange(len(computerCards))))
+                print(f"momo{computerCards, computerCardsThrown}")
+            else:
+                pass
 
-        def checkHandWinner(hand, playerCardsThrown, computerCardsThrown):
 
+        def checkHandWinner(handIndex, playerCardsThrown, computerCardsThrown):
+            print("checkHandWinner")
             nonlocal playerPoints
             nonlocal computerPoints
             nonlocal turn
 
-            print(f"\n{turn} ganó la {hand + 1} mano\n")
-            print(playerCards, playerCardsThrown)
-            print(computerCards, computerCardsThrown)
-            print(f"Tiraste el {playerCardsThrown[hand]}")
-            print(f"La computadora tiró {computerCardsThrown[hand]}")
 
-            if CARD_VALUES[playerCardsThrown[hand]] == CARD_VALUES[computerCardsThrown[hand]]:
+            if CARD_VALUES[playerCardsThrown[handIndex]] == CARD_VALUES[computerCardsThrown[handIndex]]:
                 print(f"Empate, turno de {turn}")
                 playerPoints += 1
                 computerPoints += 1
-            if CARD_VALUES[playerCardsThrown[hand]] > CARD_VALUES[computerCardsThrown[hand]]:
+                checkWinner(playerPoints, computerPoints, turn, handIndex)
+            elif CARD_VALUES[playerCardsThrown[handIndex]] > CARD_VALUES[computerCardsThrown[handIndex]]:
+                print(f"Tiraste el {playerCardsThrown[handIndex]}")
+                print(f"La computadora tiró {computerCardsThrown[handIndex]}")
                 turn = "computer"
+                computerPoints += 1
+                checkWinner(playerPoints, computerPoints, turn, handIndex)
             else:
+                print(f"Tiraste el {playerCardsThrown[handIndex]}")
+                print(f"La computadora tiró {computerCardsThrown[handIndex]}")
                 turn = "player"
-                game(turn, hand + 1, data)
+                playerPoints += 1
+                checkWinner(playerPoints, computerPoints, turn, handIndex)
 
 
+    
+        def checkWinner(playerPoints, computerPoints, turn, handIndex):
+            print("checkWinner")
+            if playerPoints == 2:
+                #print(f"Player WINS!")
+                again()
+            elif computerPoints == 2:
+                #print(f"Computer WINS!")
+                again()
+            else:
+                print("im 0")
+                game(turn, handIndex + 1, data)
+
+            print(f"checkWinner: Player points: {playerPoints}")
+            print(f"checkWinner: {turn} ganó la {handIndex + 1} mano")
+            print(f"checkWinner: Player Cards: {playerCards}, Thrown: {playerCardsThrown}")
+            print(f"checkWinner: Computer Cards: {computerCards}, Thrown: {computerCardsThrown}")
+            print(f"checkWinner: Tiraste el {playerCardsThrown[handIndex]}")
+            print(f"checkWinner: La computadora tiró {computerCardsThrown[handIndex]}")
+
+        def again():
+            nonlocal turn
+            playAgainInput = input(f"\nPlay again?\nPress Y to play again or Q to quit.\n")
+            playAgain = str(playAgainInput)
+            print(f"again: playAgain.capitalize {playAgain}")
+
+            if playAgain == "y":
+                print("playing again")
+                truco()
+            elif playAgain == "q":
+                print("byebye")
+                quit()
+            else:
+                print("Invalid input, press Y to play again or Q to quit.\n")
+                again()
+        playerThrowCard(handIndex)
+    
+    if turn == 'player':
+        print("im 1")
+        game(turn, 0, data)
+    elif turn == 'computer':
+        print("im 2")
+        game(turn, 0, data)
+    else:
+        pass
         
-        if turn == 'player':
-          playerThrowCard(hand)
-          computerThrowCard(computerCardsThrown)
-            
-        elif turn == 'computer':
-            computerThrowCard(computerCardsThrown)
-            playerThrowCard(hand)
+        print(f"game")
+    print(f"truco")
 
-
-    game(turn, hand, deal(2))
 
 
 truco()
