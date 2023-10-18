d360202b6c240b34f331caf04d3ac644649cf7f6
Felimikli <miklikowskifr@gmail.com>
2023-10-18 00:21:43 -0300

    first envido implementation

---
+++ envido.py
@@ -0,0 +1,5 @@
+from icecream import ic
+
+
+def envido(playerCards, computerCards, envidoInput):
+    return ic(playerCards, computerCards)
--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from icecream import ic
 import random
-
-espada = ['e1', 'e2', 'e3', 'e4', 'e5',
-          'e6', 'e7', 'e10', 'e11', 'e12']
-basto = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b10', 'b11', 'b12']
-oro = ['o1', 'o2', 'o3', 'o4', 'o5', 'o6', 'o7', 'o10', 'o11', 'o12']
-copa = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c10', 'c11', 'c12']
-CARD_VALUES = {'e1': 14, 'b1': 13, 'e7': 12, 'o7': 11, 'e3': 10, 'b3': 10, 'o3': 10, 'c3': 10, 'e2': 9, 'b2': 9, 'o2': 9, 'c2': 9, 'o1': 8, 'c1': 8, 'e12': 7, 'b12': 7, 'o12': 7, 'c12': 7, 'e11': 6, 'b11': 6,
-               'o11': 6, 'c11': 6, 'e10': 5, 'b10': 5, 'o10': 5, 'c10': 5, 'b7': 4, 'c7': 4, 'e6': 3, 'b6': 3, 'o6': 3, 'c6': 3, 'e5': 2, 'b5': 2, 'o5': 2, 'c5': 2, 'e4': 1, 'b4': 1, 'o4': 1, 'c4': 1, }
-cards = espada + basto + oro + copa
+from cards import cards, CARD_VALUES
+from envido import envido
 
 
 def deal(numPlayers):
@@ -23,9 +16,6 @@ def deal(numPlayers):
 
 def getPlayerInput(playerCards):
     numPlayerCards = len(playerCards)
-    # Showing the cards:
-    for card in playerCards:
-        print(f"{playerCards.index(card) + 1}. {card}")
     try:
         if numPlayerCards == 3:
             playerInput = int(input("Press (1, 2 or 3) to throw a card\n"))
@@ -35,19 +25,32 @@ def getPlayerInput(playerCards):
             playerInput = int(input("Press (1) to throw a card\n"))
 
         if playerInput < 1 or playerInput > 3:
-            print(f"Invalid input, enter (1 to {numPlayerCards})\n")
-            getPlayerInput(playerCards)
+            print(f"Invalid input:")
+            return getPlayerInput(playerCards)
         else:
             return playerInput - 1
     except ValueError:
-        print(f"Invalid input, enter (1 to {numPlayerCards})\n")
-        getPlayerInput(playerCards)
+        print(f"Invalid input:")
+        return getPlayerInput(playerCards)
 
 
 def getComputerInput(computerCards):
     if computerCards != []:
         computerInput = random.randint(0, len(computerCards) - 1)
         return computerInput
+
+
+def getEnvidoInput(playerCards, computerCards, startingTurn):
+    envidoInput = str(
+        input('Type "ENVIDO", "REAL ENVIDO", "FALTA ENVIDO" o "PASO"\n')).lower()
+    validInputs = ['envido', 'real envido', 'falta envido']
+    if envidoInput in validInputs:
+        return envidoInput
+    elif envidoInput == 'paso':
+        pass
+    else:
+        print('Invalid input')
+        getEnvidoInput(playerCards, computerCards, startingTurn)
 
 
 def playAgain():
@@ -73,6 +76,9 @@ def truco():
     handCards = deal(numPlayers)
     playerCards = handCards['player1']
     computerCards = handCards['player2']
+    print("-------------------\n| Your cards are: |\n-------------------")
+    for card in playerCards:
+        print(f"{playerCards.index(card) + 1}. | {card} |")
 
     for round in range(3):
         print(f"Hand: {round + 1}")
@@ -83,6 +89,10 @@ def truco():
 
         while True:
             if turn == 'player':
+                if round == 0:
+                    envidoInput = getEnvidoInput(
+                        playerCards, computerCards, startingTurn)
+
                 playerThrownCardIndex = getPlayerInput(playerCards)
                 playerThrownCard = playerCards.pop(playerThrownCardIndex)
                 turn = 'computer'
@@ -97,7 +107,7 @@ def truco():
                     playerPoints += 2
                     turn = 'player'
                     print(f"hand: {round + 1} won by {turn}\n")
-                    
+
                 elif CARD_VALUES[playerThrownCard] < CARD_VALUES[computerThrownCard]:
                     computerPoints += 2
                     turn = 'computer'
